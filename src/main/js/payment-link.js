ba003a3f114592e9e3efc6ce29d4440e7cccedec
Julien Lengrand-Lambert <julien@lengrand.fr>
2022-10-27 23:34:34 +0200

    Dynamic badges

--- src/main/js/payment-link.js
+++ src/main/js/payment-link.js
@@ -1,6 +1,7 @@
 import {LitElement, html, css} from 'lit';
 
 import { badge } from '@vaadin/vaadin-lumo-styles/badge.js';
+import '@vaadin/icons';
 
 export class PaymentLink extends LitElement {
     static properties = {
@@ -42,9 +43,44 @@ export class PaymentLink extends LitElement {
                 <div><a href="${this.link.url}">${this.link.id}</a> </div>
                 <div><p>${this.link.amountValue} ${this.link.amountCurrency} </p></div>
                 <div><p class="expiration">Expires : ${new Date(this.link.expiresAt).toLocaleString()} </p></div>
-                <div class="badge"><span theme="badge">${this.link.status}</span></div>
+                <div class="badge">${this._returnBadge(this.link.status)}</div>
             </div>
         `;
+    }
+
+    _returnBadge(status){
+        switch(status) {
+            case "active":
+                return html`<span theme="badge">
+                    <vaadin-icon icon="vaadin:clock" style="padding: 0.25rem"></vaadin-icon>
+                    <span>${status}</span>
+                </span>`;
+            case "completed":
+                return html`<span theme="badge success">
+                    <vaadin-icon icon="vaadin:check" style="padding: 0.25rem"></vaadin-icon>
+                    <span>${status}</span>
+                </span>`;
+            case "expired":
+                return html`<span theme="badge error">
+                    <vaadin-icon icon="vaadin:exclamation-circle-o" style="padding: 0.25rem"></vaadin-icon>
+                    <span>${status}</span>
+                </span>`;
+            case "paid":
+                return html`<span theme="badge success">
+                    <vaadin-icon icon="vaadin:check" style="padding: 0.25rem"></vaadin-icon>
+                    <span>${status}</span>
+                </span>`;
+            case "paymentPending":
+                return html`<span theme="badge">
+                    <vaadin-icon icon="vaadin:clock" style="padding: 0.25rem"></vaadin-icon>
+                    <span>${status}</span>
+                </span>`;
+            default:
+                return html`<span theme="badge contrast">
+                    <vaadin-icon icon="vaadin:hand" style="padding: 0.25rem"></vaadin-icon>
+                    <span>${status}</span>
+                </span>`;
+        }
     }
 
 }
